13f8fca82d17a254616d7b38ea0273305f36d8aa
Isabella Romero <isabella.romero@utec.edu.pe>
2024-05-12 22:46:45 -0500

    templates

--- ExtendibleHashing.cpp
+++ ExtendibleHashing.cpp
@@ -3,16 +3,20 @@
 //
 
 #include "ExtendibleHashing.h"
+#include <fstream>
+#include <cstdio>
 
-void ExtendibleHashing::saveBucket(ExtendibleHashing::Bucket *bucket, int index) {
+template<typename T>
+void ExtendibleHashing<T>::saveBucket(typename ExtendibleHashing<T>::Bucket *bucket, int index) {
     fstream file(this->fileName, ios::in | ios::out | ios::binary);
     file.seekp(index * sizeof(Bucket));
     file.write((char *) bucket, sizeof(Bucket));
     file.close();
 }
 
-ExtendibleHashing::Bucket *ExtendibleHashing::loadBucket(int index) {
-fstream file(this->fileName, ios::in | ios::binary);
+template<typename T>
+typename ExtendibleHashing<T>::Bucket *ExtendibleHashing<T>::loadBucket(int index) {
+    fstream file(this->fileName, ios::in | ios::binary);
     auto *bucket = new Bucket();
     file.seekg(index * sizeof(Bucket));
     file.read((char *) bucket, sizeof(Bucket));
@@ -20,7 +24,8 @@ fstream file(this->fileName, ios::in | ios::binary);
     return bucket;
 }
 
-void ExtendibleHashing::splitBucket(int index) {
+template<typename T>
+void ExtendibleHashing<T>::splitBucket(int index) {
     auto *bucket = loadBucket(index);
     auto *newBucket = new Bucket();
 
